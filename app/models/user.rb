2a19d3f706de7c1b13bcaf9303b661c52aee809d
asakobear11 <asafushi31@gmail.com>
2023-07-22 19:18:37 +0900

    モデルにバリデーション記述2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates [:nickname, :last_name_kanji, :first_name_kanji, :last_name_kana, :first_name_kana, :birthday] presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end
 
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }  
 
-  validates [:last_name_kanji, :first_name_kanji] format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
-
-  validates [:last_name_kana, :first_name_kana] format: { with: /\A[ァ-ヶー－]+\z/ }
 end
