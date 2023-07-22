f698896a1d55765e25283e534825a095134f7b8f
asakobear11 <asafushi31@gmail.com>
2023-07-22 18:42:58 +0900

    モデルにバリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates [:nickname, :last_name_kanji, :first_name_kanji, :last_name_kana, :first_name_kana, :birthday] presence: true
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }  
+
+  validates [:last_name_kanji, :first_name_kanji] format: { with: /\A[ぁ-んァ-ヶ一-龥]/+\z }
+
+  validates [:last_name_kana, :first_name_kana] format: { with: /\A[ァ-ヶー－]+\z/ }
 end
