b9bcea17a1328457d4966a8989e009224c65e375
asakobear11 <asafushi31@gmail.com>
2023-08-04 01:11:06 -0700

    ルーティング・アクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new 
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :description, :details_category_id, :details_condition_id, :delivery_cost_id, :prefecture_id, :delivery_date_id, :price)
+  end
+
 end
