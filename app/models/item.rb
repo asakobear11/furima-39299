df5adc07d3f34539a23821684cd6d15e383d82f6
asakobear11 <asafushi31@gmail.com>
2023-08-06 22:39:22 -0700

    ActiveHashの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :order
+  belongs_to :details_category
+  belongs_to :details_condition
+  belongs_to :delivery_cost
+  belongs_to :prefecture
+  belongs_to :delivery_date
+
+  validates :item_name, :description, :details_category_id, :details_condition_id, :delivery_cost_id, :prefecture_id, :delivery_date_id, :price, presence: true
+
+  validates :details_category_id, :details_condition_id, :delivery_cost_id, :prefecture_id, :delivery_date_id, numericality: { other_than: 0 , message: "can't be blank"} 
+
+  
+
 end
