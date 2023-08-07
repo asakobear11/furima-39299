81b26e25713576e22497f827d15d8311d592c50a
asakobear11 <asafushi31@gmail.com>
2023-08-06 23:38:44 -0700

    画像添付の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
 
-  validates :item_name, :description, :details_category_id, :details_condition_id, :delivery_cost_id, :prefecture_id, :delivery_date_id, :price, presence: true
+  validates :image, :item_name, :description, :details_category_id, :details_condition_id, :delivery_cost_id, :prefecture_id, :delivery_date_id, :price, presence: true
 
   validates :details_category_id, :details_condition_id, :delivery_cost_id, :prefecture_id, :delivery_date_id, numericality: { other_than: 0 , message: "can't be blank"} 
 
