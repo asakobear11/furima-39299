8b2550ffede07e46567ec825698f40bfab7e948f
asakobear11 <asafushi31@gmail.com>
2023-08-10 21:10:20 -0700

    プルリクエスト修正１

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,16 +13,15 @@ class Item < ApplicationRecord
     validates :image
     validates :item_name
     validates :description
-    validates :price,                format: { with: /\A\d+\z/, message: 'は半角数値のみ入力してください' },
-                                     numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
   with_options presence: true, numericality: { only_integer: true, other_than: 0 } do
-    validates :details_category_id,  format: { with: /\A([1-9]|10)\z/ }
-    validates :details_condition_id, format: { with: /\A[1-6]\z/ }
-    validates :delivery_cost_id,     format: { with: /\A[1-2]\z/ }
-    validates :prefecture_id,        format: { with: /\A([1-9]|[1-3][0-9]|4[0-7])\z/ }
-    validates :delivery_date_id,     format: { with: /\A[1-3]\z/ }
+    validates :details_category_id,  numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+    validates :details_condition_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 6 }
+    validates :delivery_cost_id,     numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 2 }
+    validates :prefecture_id,        numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 47 }
+    validates :delivery_date_id,     numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 3 }
   end
 
 end
