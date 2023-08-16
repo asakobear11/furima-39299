5d726722133332f8cdd295b309d75430260ca138
asakobear11 <asafushi31@gmail.com>
2023-08-16 00:22:08 -0700

    プルリクエスト前修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,14 +24,14 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    return unless @item.user_id != current_user.id || @item.sold?
+    return unless @item.user_id != current_user.id
 
     redirect_to root_path
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
