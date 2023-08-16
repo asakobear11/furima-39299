f3fccb271491f071b7d5a0c9ce2f52c801a2d420
asakobear11 <asafushi31@gmail.com>
2023-08-16 01:08:36 -0700

    プルリクエスト修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -37,7 +37,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    return unless @item.destroy
+    return unless @item.destroy || @item.user_id != current_user.id
 
     redirect_to root_path
   end
