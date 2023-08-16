9a02c7648ce63dd28af0fcd7e72a0c578d071ca3
asakobear11 <asafushi31@gmail.com>
2023-08-16 00:00:39 -0700

    商品情報編集機能の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -22,10 +22,28 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if @item.user_id != current_user.id || @item.sold?
+      redirect_to root_path
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :item_name, :description, :details_category_id, :details_condition_id,
                                  :delivery_cost_id, :prefecture_id, :delivery_date_id, :price).merge(user_id: current_user.id)
   end
+
+  
 end
