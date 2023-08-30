19aba0fa68460ecdfcf63fe1c7cb378893c9f3eb
asakobear11 <asafushi31@gmail.com>
2023-08-30 02:13:26 -0700

    Formオブジェクトを用いた実装

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,9 +1,27 @@
 class AddressesController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
-    @item = Item.find_by(id: params[:format])
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+    # @item = Item.find_by(id: params[:format])
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id)
   end
 
   
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :order
 
-  with_options presence: true do
-    validates :postcode,      format: { with: /\A\d{3}[-]\d{4}\z/ }
-    validates :prefecture_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 47 }
-    validates :city
-    validates :block
-    validates :phone_number,  format: { with: /\A\d{10,11}\z/ }
+
   end
 end
