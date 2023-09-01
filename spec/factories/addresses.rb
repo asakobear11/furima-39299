bd70fe61eaff8246a4b6f3006f412a9c45b3e5cc
asakobear11 <asafushi31@gmail.com>
2023-09-01 02:57:12 -0700

    テストコードの実装

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,12 +1,5 @@
 FactoryBot.define do
   factory :address do
 
-    postcode       { Faker::Address.postcode }
-    prefecture_id  { rand(1..47) }
-    city           { Faker::Address.city }
-    block          { Faker::Address.street_address }
-    phone_number   { Faker::PhoneNumber.phone_number }
-    token          {"tok_abcdefghijk00000000000000000"}
-
   end
 end
